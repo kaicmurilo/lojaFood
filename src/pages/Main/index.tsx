33246a7667c169964ff659e4c97f8e8058d302a9
Kaic Nunes <kaicmurilo@live.com>
2023-03-25 12:43:26 -0400

    adicionando pages e alinhando logo

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,16 @@
+import { Outlet } from 'react-router-dom'
 import { Sidebar } from '../../components/Sidebar'
 import { Container } from './styles'
+import logoImg from '../../assets/logo.svg'
 
 export default function Main() {
   return (
     <Container>
       <Sidebar></Sidebar>
-      <h1>Main</h1>
+      <section>
+        <img src={logoImg} alt='Logo do app' />
+        <Outlet></Outlet>
+      </section>
     </Container>
   )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 
 import MainPage from './pages/Main'
+import BurgersPage from './pages/Burgers'
+import PizzasPage from './pages/Pizzas'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<MainPage />} />
+      <Route path='/' element={<MainPage />}>
+        <Route path='/' element={<BurgersPage />} />
+        <Route path='/pizzas' element={<PizzasPage />} />
+      </Route>
     </Routes>
   )
 }
